bc0de86d67c36d9b92b267a8575d5d75fc144eff
maddog666 <aashrit93@gmail.com>
2019-06-10 09:36:26 +0530

    yaay

--- server.js
+++ server.js
@@ -19,7 +19,13 @@ const BookType = new GraphQLObjectType({
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
     name: { type: GraphQLNonNull(GraphQLString) },
-    authorId: { type: GraphQLNonNull(GraphQLInt) }
+    authorId: { type: GraphQLNonNull(GraphQLInt) },
+    author: {
+      type: AuthorType,
+      resolve: book => {
+        return authors.find(author => author.id === book.authorId);
+      }
+    }
   })
 });
 
@@ -28,7 +34,13 @@ const AuthorType = new GraphQLObjectType({
   description: "This represents a author of a book",
   fields: () => ({
     id: { type: GraphQLNonNull(GraphQLInt) },
-    name: { type: GraphQLNonNull(GraphQLString) }
+    name: { type: GraphQLNonNull(GraphQLString) },
+    books: {
+      type: new GraphQLList(BookType),
+      resolve: author => {
+        return books.filter(book => book.authorId === author.id);
+      }
+    }
   })
 });
 
